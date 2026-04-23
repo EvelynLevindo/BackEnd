246d136c8aa9047bf27df48e8379a69258497533
Evelyn Levindo <levindoevelyn2@gmail.com>
2026-04-23 16:55:25 -0300

    php

---
+++ PHP/Introdução/S1_R1_AT1/index.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1>BEM-VINDO</h1>";
+    echo "<p>Somos o site da empresa!</p>";
+    echo "<p>🏭👩🏻‍💼</p>";
+?>
---
+++ PHP/Introdução/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Primeiro PHP</title>
+</head>
+<body>
+    <h1>PHP Abaixo</h1>
+    <?php
+        echo "<h2>Bem Versátil</h2>";
+        echo "<h3>Programando em HTML</h3>";
+        echo "Olá Visitante! 👽";
+        // COM
+        ## ENT
+        /* ARIO */
+    ?>
+</body>
+</html>
