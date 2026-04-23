99ed368d1be9748aebd8cdc75436ac3cdce835d2
Evelyn Levindo <levindoevelyn2@gmail.com>
2026-04-23 17:06:09 -0300

    att php 2

---
+++ PHP/Atividades/S2_R1_AT1/index.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1>BEM-VINDO</h1>"; // Título principal da página
+    echo "<p>Somos o site da empresa!</p>"; // Parágrafo
+    echo "<p>🏭👩🏻‍💼</p>"; // Emojis no parágrafo
+?>
--- PHP/Introdução/S1_R1_AT1/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo "<h1>BEM-VINDO</h1>";
-    echo "<p>Somos o site da empresa!</p>";
-    echo "<p>🏭👩🏻‍💼</p>";
-?>
